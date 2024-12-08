aa98afa3263a7699d710a4dd82ecfd66b4d47dc2
Alphousseyni Mane <papealphou1986@gmail.com>
2024-12-08 15:48:31 +0000

    commit 1

---
+++ services/config-server-service/src/main/java/com/atongbatechs/gmp/configserverservice/ConfigServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atongbatechs.gmp.configserverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerServiceApplication.class, args);
+	}
+
+}
